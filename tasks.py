6467a9ba9bd09454ba3adfad22cb785e12534b96
Janusz Prusaczyk (JJPR) <jjpr@demant-technology.com>
2019-02-12 18:10:35 +0100

    Add schema validation

--- tasks.py
+++ tasks.py
@@ -2,6 +2,7 @@ import os
 import glob
 import shutil
 import json
+import jsonschema
 import collections
 
 from invoke import task
@@ -9,6 +10,7 @@ from invoke import task
 src_dir = os.path.abspath('./src')
 tmp_dir = os.path.abspath('./tmp')
 out_dir = os.path.abspath('./out')
+tool_dir = os.path.abspath('./tool')
 
 languages = os.listdir(src_dir)
 
@@ -38,6 +40,40 @@ Text = collections.namedtuple('Text', [
     'audio',
     'video',
     'note'])
+
+@task
+def validate(c):
+    schema = {}
+    #print('Loading schema files...')
+    
+    try:
+        for filename in glob.glob(
+            "{directory}/schema/**/*.json".format(directory=tool_dir),
+            recursive=True):
+            format = os.path.splitext(os.path.basename(filename))[0]
+            with open(filename, encoding='utf-8') as f:
+                schema[format] = json.loads(f.read())
+    except Exception as e:
+        print('Failed to load schema file: {message}'.format(message = str(e)))
+        
+    for filename in glob.glob(
+        "{directory}/**/*.json".format(directory=src_dir),
+        recursive=True):
+        #print("Validating file: {filename}".format(filename = filename))
+        try:
+            with open(filename, encoding='utf-8') as f:
+                jsondata = json.loads(f.read())
+            if not 'meta' in jsondata:
+                raise Exception("Missing required 'meta' key in: {filename}".format(filename = filename))
+            if not 'format' in jsondata['meta']:
+                raise Exception("Missing required 'format' key in: {filename}".format(filename = filename))
+            if not 'version' in jsondata['meta']:
+                raise Exception("Missing required 'version' key in: {filename}".format(filename = filename))
+            jsonschema.validate(jsondata, schema['{format}-{version}'.format(format=jsondata['meta']['format'], version=jsondata['meta']['version'])])
+        except jsonschema.exceptions.ValidationError as e:
+            print('{filename}: {message}'.format(filename = filename, message = e.message))
+        except Exception as e:
+            print('{filename}: {message}'.format(filename = filename, message = str(e)))
 
 @task
 def clean(c, output = False):
